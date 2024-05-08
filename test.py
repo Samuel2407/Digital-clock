613baf178ffd5c62b588a13916e27ed7a833d02f
Samuel Perez <samuel.perez@cua.uam.mx>
2024-05-08 13:32:03 -0600

    Codigo de digital clock

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 """This module holds functions to test the functionality of this project
-    """
+"""
     
 from digital_clock import DigitalClock
 from utime import sleep
@@ -7,6 +7,10 @@ from utime import sleep
 def clock_test() -> None:
     clock = DigitalClock()
     while True:
+        h, m, s = clock.get_time()
+        time = f'{h:02} : {m:02} : {s:02}'
+        print(time)
         sleep(1)
-        pass
+        clock.increment()
+        
         
